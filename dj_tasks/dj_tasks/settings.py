3a52d6b167536b518d6c6e018d95b40aac8f31e8
canislatranscoxus <canislatranscoxus@gmail.com>
2021-02-05 18:03:39 -0600

    static_root

--- dj_tasks/dj_tasks/settings.py
+++ dj_tasks/dj_tasks/settings.py
@@ -14,7 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 # ------------------------------------------------------------------------------------------------
 
