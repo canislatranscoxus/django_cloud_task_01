c75aca56ee7572c5ef3b77728cb51bab8dad9066
canislatranscoxus <canislatranscoxus@gmail.com>
2021-02-05 21:56:16 -0600

    uri

--- dj_tasks/app_queue/Qmessenger.py
+++ dj_tasks/app_queue/Qmessenger.py
@@ -26,7 +26,7 @@ class Qmessenger:
 
     def add_gae( self, payload ):
         '''params is a dictionary '''
-        relative_uri = '/app_queue/handler_animals'
+        relative_uri = '/handler_animal'
         
         if 'url' in payload:
             relative_uri = payload[ 'url' ]
